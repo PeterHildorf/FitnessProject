113b1ef2ebb713dce50b00fa3d59d83158b64522
Wame Gassama <161361272+WameGassama@users.noreply.github.com>
2025-04-01 11:45:08 +0200

    Created the login page

--- FitnessProject/View/LoginView.swift
+++ FitnessProject/View/LoginView.swift
@@ -2,6 +2,62 @@
 //  LoginView.swift
 //  FitnessProject
 //
-//  Created by Peter Hannibal Hildorf on 01/04/2025.
+//  Created by Wame Gassama on 01/04/2025.
 //
 
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack(spacing: 70) {
+            Image("authentication")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: 300, maxHeight: 300)
+                .padding(.top, 100)
+            VStack(spacing: 20) {
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Text("Get started").foregroundColor(Color.white).font(.system(size: 18, weight: .bold, design: .rounded)).frame(maxWidth: .infinity)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .background(Color.blue)
+                    .clipShape(.rect(cornerRadius: 20))
+                }
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Text("Login").foregroundColor(Color.blue).font(.system(size: 18, weight: .bold, design: .rounded))
+                            .frame(maxWidth: .infinity)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .background(Color.white)
+                    .clipShape(.rect(cornerRadius: 20))
+                }
+                HStack {
+                    Text("New around here?")
+                    Button {
+                        
+                    } label: {
+                        Text("Sign up")
+                    }
+                }
+                .padding(.top, 20)
+                .font(.system(size: 16, weight: .light, design: .rounded))
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
+        .background(.gray.opacity(0.2))
+        
+    }
+}
+
+#Preview {
+    LoginView()
+}
